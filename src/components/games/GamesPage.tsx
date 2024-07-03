2bfdca9cff558b5b6bb19fc190252397b7afb5bd
manolopzy <manolopeng@gmail.com>
2024-07-03 20:14:17 +0200

    games board

---
+++ src/components/games/GamesPage.tsx
@@ -0,0 +1,13 @@
+function GamesPage() {
+    
+    return (
+        <div className="games">
+            <div>Arithmetic Game</div>
+            <div>Puzzle Game</div>
+            <div>Snake Game</div>
+            <div>Galaxy Game</div>
+        </div>
+    );
+}
+
+export default GamesPage;
--- src/components/games/arithmetic/PlayBoard.tsx
+++ src/components/games/arithmetic/PlayBoard.tsx
@@ -2,59 +2,72 @@ import axios, { AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 import { SERVER_URL, RANDOM_ARITHEMETIC_OPERATION } from "../../../Constants";
 
-
 function PlayBoard() {
-    const [operator, setOperator] = useState<string>("");
-    const [operation, setOperation] = useState<string>("");
-    useEffect(() => {
-        //'https://domain.com/path/?param1=value1&param2=value2'
-        
-        axios.get(SERVER_URL + RANDOM_ARITHEMETIC_OPERATION + operator).then((response: AxiosResponse) => {
-            console.log(response.data);
-        }).catch((error) => {
-            console.log(error);
+  const [operator, setOperator] = useState<string>("+");
+  const [operation, setOperation] = useState<string>("");
+  const onOperatorChange = () => {
 
-        }
-        );
-    })
-    return (
-        <>
+  };
+  useEffect(() => {
+    //'https://domain.com/path/?param1=value1&param2=value2'
+
+    axios
+      .get(SERVER_URL + RANDOM_ARITHEMETIC_OPERATION + operator)
+      .then((response: AxiosResponse) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+  return (
+    <div className="playboard">
         <div className="row">
-                <div className="col-md-12">
-                    <h1 className="text-center">Welcome to Social Arithmetic</h1>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-6">
-                    <h3 className="text-center">This is your challenge for today:</h3>
-                    <h1 className="text-center">
-                        <span className="arithmetic-a"></span>
-                        <select id="arithmetic">
-                            <option value="addition">+</option>
-                            <option value="subtraction">-</option>
-                            <option value="multiplication">×</option>
-                            <option value="division">÷</option>
-                        </select>
-                        <span className="arithmetic-b"></span>
-                    </h1>
-                    <p>
-                        <form id="attempt-form">
-                            <div className="form-group">
-                                <label htmlFor="result-attempt">Result?</label>
-                                <input type="text" name="result-attempt" id="result-attempt" className="form-control" />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="user-alias">Your alias:</label>
-                                <input type="text" name="user-alias" id="user-alias" className="form-control" />
-                            </div>
-                            <input type="submit" value="Check" className="btn btn-default" />
-                        </form>
-                    </p>
-                    <div className="result-message"></div>
-                </div>
-            </div>
-        </>
-    );
+        <div className="col-md-12">
+          <h1 className="text-center">Welcome to Social Arithmetic</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6">
+          <h3 className="text-center">This is your challenge for today:</h3>
+          <h1 className="text-center">
+            <span className="arithmetic-a"></span>
+            <select id="arithmetic">
+              <option value="addition">+</option>
+              <option value="subtraction">-</option>
+              <option value="multiplication">×</option>
+              <option value="division">÷</option>
+            </select>
+            <span className="arithmetic-b"></span>
+          </h1>
+          <p>
+            <form id="attempt-form">
+              <div className="form-group">
+                <label htmlFor="result-attempt">Result?</label>
+                <input
+                  type="text"
+                  name="result-attempt"
+                  id="result-attempt"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="user-alias">Your alias:</label>
+                <input
+                  type="text"
+                  name="user-alias"
+                  id="user-alias"
+                  className="form-control"
+                />
+              </div>
+              <input type="submit" value="Check" className="btn btn-default" />
+            </form>
+          </p>
+          <div className="result-message"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PlayBoard;
